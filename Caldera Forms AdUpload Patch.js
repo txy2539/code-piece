438a6cdba7011b586a3c27b5bfd19e6e5cf01991
Tao Yang <tao@freshop.com>
2018-09-25 13:09:43 -0400

    update solution.

--- Caldera Forms AdUpload Patch.js
+++ Caldera Forms AdUpload Patch.js
@@ -1,6 +1,14 @@
-    // Fix caldera form issue. submit seems not work when using advanced file uploader 
+    // file uploader
 
+    $(document).on("touchstart", ".cf-uploader-trigger",function(){
+        $(".cf-uploader-trigger").trigger("focus").trigger("click");
+        $(".cf-uploader-trigger").trigger("touchcancel");
+    });
+
+    // Fix caldera form issue. 
     $(document).ready(function(){
+
+        //submit seems not work when using advanced file uploader 
         $(".button").mouseup(function(){
             setTimeout(function(){
                 var uploadDisplay = $(".cf-uploader-trigger").css('display');
@@ -14,5 +22,20 @@
         $("#fld_2007293_1").click(function(){
             var copyValue = $("#fld_2007293_1").val();
             $("#fld_2007293_1").val("").val(copyValue);
+        });
+
+        $("#fld_645468_1").click(function(){
+            var copyValue = $("#fld_645468_1").val();
+            $("#fld_645468_1").val("").val(copyValue);
+        });
+
+        // store select
+        $("#fld_9970286_1").on("touchend",function(){
+            $("#fld_9970286Label").click();
+        });
+
+        // join us title 
+        $("#fld_1529543_1").on("touchend",function(){
+            $("#fld_1529543Label").click();
         });
     });
