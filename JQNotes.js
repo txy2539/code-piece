73bafafc121f67f6a75499a9795ccee6d5638cbb
Tao Yang <tao@freshop.com>
2018-10-30 15:11:45 -0400

    jquery 实例

--- JQNotes.js
+++ JQNotes.js
@@ -2,3 +2,12 @@
 修改max的值 $("#num").attr("max","值")
 获取min的值 $("#num").attr("min")
 获取max的值 $("#num").attr("max")
+
+// set charactor limit
+<script type="text/javascript">
+$(document).ready(function() { 
+   $(".audience-other-limit input").attr("maxlength","20");
+   $(".custom-order textarea").attr("maxlength","200");
+   $(".order-notes textarea").attr("maxlength","1000");
+});
+</script>
